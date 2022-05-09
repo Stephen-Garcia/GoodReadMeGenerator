033272ed8001ec89c938c3f241e9a3d094adb67d
Stephen Garcia <garciastephen5280@gmail.com>
2022-05-09 13:04:48 -0600

    worked on index.js and utils

--- index.js
+++ index.js
@@ -1,13 +1,126 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs");
+const path = require("path");
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const questions = [
+    {
+      type: "input",
+      message: "What is the title of your readme?",
+      name: "projectTitle",
+      default () {
+        return `README Generator`
+      }
+    },
+    {
+      type: "input",
+      message: `Provide a short description explaining the what, why, and how of your project.`,
+      name: "description",
+      default () {
+        return `This CLI app was created to help you make README files with ease!`
+      }
+    },
+    {
+      type: "input",
+      message: "What steps are required to install your project?",
+      name: "installation",
+      default () {
+        return `1. Clone the repo
+  \`\`\`sh
+  git clone https://github.com/your_username_/Project-Name.git
+  \`\`\`
+  2. Install NPM packages
+  \`\`\`sh
+  npm install
+  \`\`\`
+  3. Run index.js via Node
+  \`\`\`sh
+  node ./index.js
+  \`\`\``;
+      }
+    },
+    {
+      type: "input",
+      message:
+        "Provide instructions and examples for use. Include screenshots as needed.",
+      name: "usage",
+      default () {
+        return `View the code and try for yourself!`
+      }
+    },
+    {
+      type: "input",
+      message: "Placeholder for contributing, no input necessary!",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Provide information about testing performed for your project.",
+      name: "tests",
+      default () {
+        return `No testing is provided with this suite.`
+      }
+    },
+    {
+      type: "input",
+      message: "Provide email address",
+      name: "email",
+      default () {
+        return `garciastephen5280@gmail.com`
+      }
+    },
+    {
+      type: "input",
+      message: "Provide your github username",
+      name: "github",
+      default () {
+        return `Stephen-Garcia`
+      }
+    },
+    {
+      type: "input",
+      message: `List your collaborators, if any, with links to their GitHub profiles.
+          If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+          If you followed tutorials, include links to those here as well.`,
+      name: "credits",
+      default () {
+        return ``
+      }
+    },
+    {
+      type: "list",
+      message: "Select a license:",
+      name: "license",
+      choices: [
+        "GNU AGPLv3",
+        "GNU GPLv3",
+        "GNU LGPLv3",
+        "Mozilla",
+        "MIT",
+        "Apache",
+        "Open",
+      ],
+    },
+  ];
+  
+  //  Create a function to write README file
+  function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
+  
+  //  Create a function to initialize app
+  function init() {
+    inquirer.prompt(questions).then((answers) => {
+      console.log(answers);
+      writeToFile(
+        "README.md",
+        generateMarkdown({
+          ...answers,
+        })
+      );
+    });
+  }
+  
+  // Function call to initialize app
+  init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseBadge(license) {
+  if (license === "GNU AGPLv3") {
+    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+  } else if (license === "GNU GPLv3") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "GNU LGPLv3") {
+    return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+  } else if (license === "Mozilla") {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Apache") {
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else {
+    return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  }
 }
 
-module.exports = generateMarkdown;
+// Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.projectTitle} 👋
+ 
+${renderLicenseBadge(data.license)}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Description
+${data.description}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${renderLicenseBadge(data.license)}
+This application is covered by the ${data.license} license. 
+## Contributing
+${data.contributing}
+You can contribute to this project by adhering to the code of conduct as mentioned: [here](https://www.contributor-covenant.org/#:~:text=Contributor%20Covenant%20is%20a%20code,that%20distinguish%20your%20own%20community)
+## Tests
+${data.tests}
+## Questions
+✋ If you have any questions, please contact me through email or github, listed below:<br />
+<br />
+Find me on GitHub: [${data.github}](https://github.com/${data.github})<br />
+<br />
+✉️ Email me with any questions: ${data.email}<br /><br />
+🖖 This README was generated with ❤️ by [README-generator](https://github.com/${
+    data.github
+  }/README-Generator) 🔥🔥🔥
+    
+  `;
+}
